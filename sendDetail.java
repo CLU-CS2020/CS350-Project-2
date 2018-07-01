7aed88bff98841e0832cf42b87a834a3d03af6a4
Jakeobie <36485981+Jakeobie@users.noreply.github.com>
2018-07-01 10:37:27 -0700

    Create sendDetail.java

---
+++ sendDetail.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iprouter;
+
+import java.net.InetAddress;
+
+/**
+ *
+ * @author jake
+ */
+public class sendDetail {
+        
+    InetAddress destinationAddress;
+    int destinationPort;
+    IPPacket outgoingIPPacket;
+
+    public sendDetail(InetAddress destinationAddress, int destinationPort, IPPacket outgoingIPPacket) {
+        this.destinationAddress = destinationAddress;
+        this.destinationPort = destinationPort;
+        this.outgoingIPPacket = outgoingIPPacket;
+    }
+
+    public InetAddress getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public void setDestinationAddress(InetAddress destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
+
+    public int getDestinationPort() {
+        return destinationPort;
+    }
+
+    public void setDestinationPort(int destinationPort) {
+        this.destinationPort = destinationPort;
+    }
+
+    public IPPacket getOutgoingIPPacket() {
+        return outgoingIPPacket;
+    }
+
+    public void setOutgoingIPPacket(IPPacket outgoingIPPacket) {
+        this.outgoingIPPacket = outgoingIPPacket;
+    }
+    
+    
+    
+}
