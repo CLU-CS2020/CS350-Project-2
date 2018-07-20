7c834871ee2bf91eaa2222020184839587d6e5f9
kevinscaringi <40499841+kevinscaringi@users.noreply.github.com>
2018-07-19 18:41:19 -0700

    Update IPPacket.java
    updated version of the IPPacket from Matthews

--- IPPacket.java
+++ IPPacket.java
@@ -1,4 +1,4 @@
-package iprouter;
+package zzz;
 
 import java.io.Serializable;
 import java.net.InetAddress;
@@ -7,47 +7,36 @@ import java.net.InetAddress;
  *
  * @author graham
  */
-public class IPPacket implements Serializable {
+public class IPPacket implements Serializable
+{
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+	//	0 = Timer, 1 = DoExchange (not using anymore), 2 = Ping, 3 = PingReply, 4 = RouterTable, 5 = Message
+	public final int messageType;
 
-    //	0 = DoPing, 1 = DoExchange, 2 = Ping, 3 = PingReply, 4 = RouterTable, 5 = Message
-    public final int messageType;
+	public final InetAddress source;
+	public final InetAddress dest;
 
-    //	used in RouterTable
-    public final InetAddress address;
-    public final long cost;
+	//	used in RouterTable
+	public final InetAddress address;
+	public final long cost;
 
-    //	used in Message
-    public final String message;
+	//	used in Message
+	public final String message;
 
-    public IPPacket(int messageType, InetAddress address, long cost, String message) {
-        this.messageType = messageType;
-        this.address = address;
-        this.cost = cost;
-        this.message = message;
-    }
+	public IPPacket(InetAddress source, InetAddress dest, int messageType, InetAddress address, long cost, String message)
+	{
+		this.source = source;
+		this.dest = dest;
+		this.messageType = messageType;
+		this.address = address;
+		this.cost = cost;
+		this.message = message;
+	}
 
-    @Override
-    public String toString() {
-        return "IPPacket{" + "messageType=" + messageType + ", address="
-                + address + ", cost=" + cost + ", message=" + message + '}';
-    }
-
-    public int getMessageType() {
-        return messageType;
-    }
-
-    public InetAddress getAddress() {
-        return address;
-    }
-
-    public long getCost() {
-        return cost;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
+	@Override
+	public String toString()
+	{
+		return "IPPacket{" + "messageType=" + messageType + ", source=" + source + ", dest=" + dest + ", address=" + address + ", cost=" + cost + ", message=" + message + '}';
+	}
 }
