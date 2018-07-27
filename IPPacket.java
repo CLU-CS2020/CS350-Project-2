036ed6ecc238578165b155e0cf718d49bec21e12
kevinscaringi <40499841+kevinscaringi@users.noreply.github.com>
2018-07-26 18:49:07 -0700

    Update IPPacket.java

--- IPPacket.java
+++ IPPacket.java
@@ -7,36 +7,61 @@ import java.net.InetAddress;
  *
  * @author graham
  */
-public class IPPacket implements Serializable
-{
-	private static final long serialVersionUID = 1L;
+public class IPPacket implements Serializable {
 
-	//	0 = Timer, 1 = DoExchange (not using anymore), 2 = Ping, 3 = PingReply, 4 = RouterTable, 5 = Message
-	public final int messageType;
+    private static final long serialVersionUID = 1L;
 
-	public final InetAddress source;
-	public final InetAddress dest;
+    //	0 = Timer, 1 = DoExchange (not using anymore), 2 = Ping, 3 = PingReply, 4 = RouterTable, 5 = Message
+    public final int messageType;
 
-	//	used in RouterTable
-	public final InetAddress address;
-	public final long cost;
+    public final InetAddress source;
+    public final InetAddress dest;
 
-	//	used in Message
-	public final String message;
+    //	used in RouterTable
+    public final InetAddress address;
+    public final long cost;
 
-	public IPPacket(InetAddress source, InetAddress dest, int messageType, InetAddress address, long cost, String message)
-	{
-		this.source = source;
-		this.dest = dest;
-		this.messageType = messageType;
-		this.address = address;
-		this.cost = cost;
-		this.message = message;
-	}
+    //	used in Message
+    public final String message;
 
-	@Override
-	public String toString()
-	{
-		return "IPPacket{" + "messageType=" + messageType + ", source=" + source + ", dest=" + dest + ", address=" + address + ", cost=" + cost + ", message=" + message + '}';
-	}
+    // source = original source
+    // dest = original destination
+    // address = who you are sending the packet to
+    public IPPacket(InetAddress source, InetAddress dest, int messageType, InetAddress address, long cost, String message) {
+        this.source = source;
+        this.dest = dest;
+        this.messageType = messageType;
+        this.address = address;
+        this.cost = cost;
+        this.message = message;
+    }
+
+    public int getMessageType() {
+        return messageType;
+    }
+
+    public InetAddress getSource() {
+        return source;
+    }
+
+    public InetAddress getDest() {
+        return dest;
+    }
+
+    public InetAddress getAddress() {
+        return address;
+    }
+
+    public long getCost() {
+        return cost;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "IPPacket{" + "messageType=" + messageType + ", source=" + source + ", dest=" + dest + ", address=" + address + ", cost=" + cost + ", message=" + message + '}';
+    }
 }
